8eb43fd4898f09553a27698d8b45d02dca6bebca
sabriemin <sabri.emin97@gmail.com>
2025-08-04 14:27:26 +0300

    Delete evren/src directory

--- evren/src/theme.js
+++
@@ -1,8 +0,0 @@
-// src/theme.js
-
-export function toggleThemeFactory(ctx) {
-  return function toggleTheme() {
-    ctx.isDarkMode = !ctx.isDarkMode;
-    localStorage.setItem('darkMode', ctx.isDarkMode);
-  };
-}
--- evren/src/timeline.js
+++
@@ -1,8 +0,0 @@
-// src/timeline.js
-
-export function toggleTimelineFactory(ctx) {
-  return function toggleTimeline() {
-    ctx.timelineMode = !ctx.timelineMode;
-    ctx.applyFilters();
-  };
-}
