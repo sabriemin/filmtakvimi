2a038f973a49e1b723a2f5aa741c0018b18cdcf7
sabriemin <sabri.emin97@gmail.com>
2025-08-04 17:24:21 +0300

    Add files via upload

--- evren/src/app.js
+++ evren/src/app.js
@@ -1,3 +1,4 @@
+
 import { setupNetwork } from './network.js';
 import { applyFilters, initFilters } from './filters.js';
 
@@ -23,57 +24,22 @@ export function app() {
     lang: 'tr',
 
     edgeColors: {
-      "devam": "#2980b9",
-      "ön hikaye": "#e67e22",
-      "yan hikaye": "#8e44ad",
-      "evren geçişi": "#c0392b",
-      "görsel gönderme": "#7f8c8d",
-      "karakter göndermesi": "#27ae60",
-      "kurumsal gönderme": "#6e4b25",
-      "zaman çizgisi bağlantısı": "#1abc9c",
-      "karakter geçişi": "#2ecc71",
-      "tematik benzerlik": "#f1c40f",
-      "duygu ve bilinç teması": "#9b59b6",
-      "konseptsel devam": "#34495e",
-      "şehir yaşamı paralelliği": "#d35400",
-      "iç film/karakter kökeni": "#7d3c98",
-      "multiverse birleşmesi": "#e84393",
-      "paralel Kang anlatımı": "#16a085"
+      "devam": "#2980b9"
     },
 
     init() {
       this.loadData();
-      this.isDarkMode = localStorage.getItem('darkMode') === 'true';
-      this.lang = localStorage.getItem('lang') || 'tr';
+      this.isDarkMode = true;
       initFilters(this);
     },
 
     async loadData() {
-      const evrenler = [
-        "avatar_last_airbender",
-        "avatar_pandora",
-        "dc",
-        "harrypotter",
-        "marvel",
-        "matrix",
-        "middleearth",
-        "pixar",
-        "starwars"
-      ];
-      let nodesTemp = [];
-      let edgesTemp = [];
-      for (const evren of evrenler) {
-        const res = await fetch('data/' + evren + '.json');
-        const data = await res.json();
-        nodesTemp = nodesTemp.concat(data.nodes.map(n => ({...n, universe: evren})));
-        edgesTemp = edgesTemp.concat(data.edges);
-      }
-      this.allNodes = nodesTemp;
-      this.allEdges = edgesTemp;
-
+      const res = await fetch('data/marvel.json');
+      const data = await res.json();
+      this.allNodes = data.nodes;
+      this.allEdges = data.edges;
       this.universeList = [...new Set(this.allNodes.map(n => n.universe))];
       this.edgeTypeList = [...new Set(this.allEdges.map(e => e.type))].filter(t => t);
-
       this.network = setupNetwork(this);
       applyFilters(this);
     },
@@ -85,22 +51,12 @@ export function app() {
 
     toggleTheme() {
       this.isDarkMode = !this.isDarkMode;
-      localStorage.setItem('darkMode', this.isDarkMode);
     },
 
     formatUniverseName(name) {
-      const map = {
-        "avatar_last_airbender": "Avatar: Son Hava Bükücü",
-        "avatar_pandora": "Avatar (Pandora)",
-        "dc": "DC",
-        "marvel": "Marvel",
-        "harrypotter": "Harry Potter",
-        "matrix": "Matrix",
-        "middleearth": "Orta Dünya",
-        "pixar": "Pixar",
-        "starwars": "Star Wars"
-      };
-      return map[name] || name;
+      return name;
     }
   }
 }
+
+window.app = app;
